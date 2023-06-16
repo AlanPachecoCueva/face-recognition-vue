a4eb07334ee3e5fb8bd518aa3bf2314901bdf9f9
Alan Pacheco <alan.cueva@yahoo.es>
2023-06-16 18:53:29 -0500

    Cambios en login

--- src/views/authView.vue
+++ src/views/authView.vue
@@ -1,21 +1,21 @@
 <template>
-<div class="containerFake">
-  <div class="container" :class="{ 'isRegister': !logIn }">
-    <registerComponent v-if="!logIn"></registerComponent>
-    <loginComponent v-if="logIn"></loginComponent>
-    <el-button v-if="logIn" class="btn loginBtn" :class="{ 'active': logIn }" @click="switchButton()">
-      <!--Iniciar Sesión-->
-      REGISTRARSE
-    </el-button>    
-  </div>
-  </div>
-  <div class="inicioF">
-  <div class="inicio">
-  <el-button v-if="!logIn" class="btn registerBtn" :class="{ 'active': !logIn }" @click="switchButton()">
-     INICIAR SESIÓN
-    </el-button>
+  <div class="containerFake">
+    <div class="container">
+      <registerComponent v-if="!logIn"></registerComponent>
+      <loginComponent v-if="logIn"></loginComponent>
+      <el-button class="btn loginBtn" @click="switchButton()">
+        <!--Iniciar Sesión-->
+        {{ textoBtn }}
+      </el-button>
     </div>
+  </div>
+  <!-- <div class="inicioF">
+    <div class="inicio">
+      <el-button v-if="!logIn" class="btn registerBtn " :class="{ 'active': !logIn }" @click="switchButton()">
+        {{ textoBtn }}
+      </el-button>
     </div>
+  </div> -->
 </template>
 
 <script>
@@ -47,14 +47,17 @@ export default {
 </script>
 
 <style>
-
-.containerFake{
-  width: 300px; /* Ajusta el ancho del contenedor según tus necesidades */
-  height: 10px; /* Ajusta la altura del contenedor según tus necesidades */
-  margin: auto; /* Margen automático para centrar horizontalmente */
+.containerFake {
+  width: 300px;
+  /* Ajusta el ancho del contenedor según tus necesidades */
+  height: 10px;
+  /* Ajusta la altura del contenedor según tus necesidades */
+  margin: auto;
+  /* Margen automático para centrar horizontalmente */
   /* Opción 2: centrar verticalmente utilizando relleno */
-  padding-top: calc(63vh - 1px);  /* Calcula la mitad de la altura de la pantalla y resta la mitad de la altura del contenedor */
-  padding-left:16vh;
+  padding-top: calc(63vh - 1px);
+  /* Calcula la mitad de la altura de la pantalla y resta la mitad de la altura del contenedor */
+  padding-left: 16vh;
 }
 
 
@@ -66,11 +69,14 @@ export default {
   display: flex;
   justify-content: center;
   z-index: 1;
-  transform: translateY(80%); /* Adjust the vertical position */
-  }
+  transform: translateY(80%);
+  /* Adjust the vertical position */
+}
 
-.inicio {  
+.inicio {
 
+  width: 100%;
+  background-color: green;
 }
 
 
@@ -85,37 +91,44 @@ export default {
 
 .loginBtn {
   color: white;
-  margin: 10% 0% 15% 0%; /* Ajusta el margen superior para bajar el botón */
+  /* margin: 10% 0% 15% 0%; */
+  /* Ajusta el margen superior para bajar el botón */
   padding: 10px 20px;
   
+  position: relative;
+  margin-top: 10%;
+}
+
+.btn {
+  /* margin: 0 auto; */
+  bottom: 0px;
+  /* Para centrar el botón horizontalmente */
+  color: #03e9f4;
+  z-index: 20;
+  /*border: 2px solid aqua; Agrega un borde blanco de 2px */
+  text-align: center;
+  /* Para centrar el texto dentro del botón */
+
 }
 
 @media (max-width: 400px) {
+
   .loginBtn,
   .registerBtn {
-    width: 80%; /* Ajusta el ancho de los botones en dispositivos móviles */
+    width: 80%;
+    /* Ajusta el ancho de los botones en dispositivos móviles */
   }
 }
 
 .registerBtn {
+  position: relative !important;
+  bottom: 0px !important;
   color: white;
-  margin: 0 0 5% 0; /* Ajusta el margen inferior para ambos botones */
-  padding: 5px 20px;
-  padding-top: calc(1vh - 1px);
+  /* margin: 0 0 5% 0; */
+  /* Ajusta el margen inferior para ambos botones */
+  padding: 1px 20px !important;
+
+  background-color: red !important;
+  transform: translate(0px, 50%) !important;
 }
-
-.btn.active {
-  /* Estilos para cuando el botón está activo */
-  /*border: 2px solid aqua;*/
-}
-
-.btn {
-  margin: 0 auto; /* Para centrar el botón horizontalmente */
-  color: #03e9f4;
-  z-index: 20;
-  /*border: 2px solid aqua; Agrega un borde blanco de 2px */  
-  text-align: center; /* Para centrar el texto dentro del botón */
-
-}
-
 </style>
